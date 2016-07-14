ce3faf098667e06afe9f69e5f0f7d5cc2cf0d627
Steven Lin <steventlin.9@gmail.com>
2016-07-14 14:05:59 -0700

    testing react DOM

--- client/src/components/EnterCrumb.js
+++ client/src/components/EnterCrumb.js
@@ -3,18 +3,31 @@ import React from 'react';
 export class EnterCrumb extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			value: ''
+		}
 	}
+
+	handleCrumbChange(e) {
+		this.setState({
+			value: e.target.value
+		})
+	}
+
+
+
+
 
 	render() {
 		return (
-		<div>
-		  <form>
-        	<input
-          		type="text"
-          		placeholder="Enter your crumb..."
-          	/>
-      	  </form>
+	    <div>
+	    	<div>
+	    		<input type="text" id="configname" name="configname" onChange={this.handleCrumbChange.bind(this)} />
+	    	</div>
+	    	<div>
+	    		<button type="button" onClick={() => this.props.addCrumb(this.state.value)}> Try </button>
+	    	</div>
 	    </div>
-	    )
+	    );
 	}
 }
