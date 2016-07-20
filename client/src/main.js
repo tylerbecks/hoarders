6263716e113866c21d8e171090592672bf72d469
Matt Dubie <mbdubie@gmail.com>
2016-07-20 09:13:09 -0700

    demo location updating via sockets

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import io from 'socket.io-client'
+let demoSocket = io('http://localhost:8000')
+let mainSocket = io('http://localhost:3000')
+
 ReactDOM.render(
-  <App />,
+  <App mainSocket={mainSocket} demoSocket={demoSocket} />,
   document.getElementById('app')
 );
 
