28a63fd8000d2f7a3ee1b84169fda37c786a2512
Tyler Becks <tylerdbecks@gmail.com>
2016-07-26 16:24:51 -0700

    Fix bug with io dependencies

--- server/db/config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
-
-module.exports.connect = () => {
-  mongoose.connect('mongodb://root:cake@ds031995.mlab.com:31995/crumbs', (err) => {
-    err ? console.log(err) : console.log('db connected');
-  });
-};
--- server/db/config.template.js
+++ server/db/config.template.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 module.exports.connect = () => {
-  mongoose.connect('mongodb://root:cake@ds031995.mlab.com:31995/crumbs', (err) => {
+  mongoose.connect('mongodb://<username>:<password>@ds031995.mlab.com:31995/crumbs', (err) => {
     err ? console.log(err) : console.log('db connected');
   });
 };
