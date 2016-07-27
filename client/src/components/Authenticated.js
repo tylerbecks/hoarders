0e526e1786e0ed7784309c1172b0e95f2c6af80a
Tyler Becks <tylerdbecks@gmail.com>
2016-07-26 20:23:45 -0700

    Add map on authenticated page

--- client/src/components/Authenticated.js
+++ client/src/components/Authenticated.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ChatRoom } from './ChatRoom.js';
 import { OutOfChatRoom } from './OutOfChatRoom.js';
 import { Jumbotron, Button } from 'react-bootstrap';
+import OurMap from './map.js';
 
 export const Authenticated = (props) => {
   const appStyle = {
@@ -31,22 +32,41 @@ export const Authenticated = (props) => {
   const childToRender = !!props.messages ? chatRoom : outOfChatRoom;
 
 
+  // return (
+  //   <div style={appStyle}>
+  //     <Button
+  //       style={{ float: 'right' }}
+  //       bsStyle="link"
+  //       onClick={props.logOutUser}
+  //     >
+  //       Logout
+  //     </Button>
+  //     <div>
+  //       <Jumbotron>
+  //         <h1>Crumbs</h1>
+  //         <p>your local chatroom</p>
+  //       </Jumbotron>
+  //       {childToRender}
+  //     </div>
+  //   </div>
+  // );
+
+  var ourMap;
+  var mapStyle = { height: screen.height - (0.15 * screen.height) };
+
+  //render the map in all cases
+  var googleMap = <div className='map-wrapper' style={mapStyle}>
+                    <OurMap center={props.center} zoom={props.zoom} />
+                  </div>
+
+  ourMap = <div className='col-xs-12 wrapper'>
+            {googleMap}
+           </div>;
+
   return (
-    <div style={appStyle}>
-      <Button
-        style={{ float: 'right' }}
-        bsStyle="link"
-        onClick={props.logOutUser}
-      >
-        Logout
-      </Button>
-      <div>
-        <Jumbotron>
-          <h1>Crumbs</h1>
-          <p>your local chatroom</p>
-        </Jumbotron>
-        {childToRender}
-      </div>
+    <div>
+      {ourMap}
     </div>
   );
+
 };
---
+++ client/src/components/map.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import GoogleMap from 'google-map-react';
+
+
+export default class OurMap extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  createMapOptions(maps){
+      return {
+      styles: [{"featureType":"landscape","stylers":[{"hue":"#FFAD00"},{"saturation":50.2},{"lightness":-34.8},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFAD00"},{"saturation":-19.8},{"lightness":-1.8},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FFAD00"},{"saturation":72.4},{"lightness":-32.6},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FFAD00"},{"saturation":74.4},{"lightness":-18},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#00FFA6"},{"saturation":-63.2},{"lightness":38},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#FFC300"},{"saturation":54.2},{"lightness":-14.4},{"gamma":1}]}]
+    };
+  }
+
+  render() {
+    //google map component from google-map-react
+    return (
+      <GoogleMap bootstrapURLKeys={{ key: 'AIzaSyDZjkD659gGlpyUKXU14_Tomji58BSfI0A', language: 'en' }} options={this.createMapOptions} center={this.props.center} zoom={this.props.zoom} >
+        
+      </GoogleMap>
+    );
+  }
+
+};
