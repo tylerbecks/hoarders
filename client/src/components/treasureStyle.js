7164a080d00761e1bcfb8799eeae2abbc1955d65
Tyler Becks <tylerdbecks@gmail.com>
2016-07-27 11:16:49 -0700

    Add userSpot marker to map

---
+++ client/src/components/treasureStyle.js
@@ -0,0 +1,53 @@
+//size of the outer div for skull icons
+export const e_size = 30;
+
+export const skateSpotStyle = {
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  left: -e_size / 2,
+  top: -e_size / 2,
+  borderRadius: e_size,
+  content: 'url(https://www.google.com/search?q=treasure+chest+icon&biw=1295&bih=778&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjX_Zrhn5TOAhVO9mMKHSC7CaYQ_AUIBigB#tbm=isch&q=treasure+chest+icon+png&imgrc=wtjkSaVlPOC-FM%3A)',
+  textAlign: 'center',
+  fontSize: 16,
+  fontWeight: 'bold',
+  padding: 4,
+  cursor: 'pointer',
+};
+
+export const skateSpotHoverStyle = {
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  left: -e_size / 2,
+  top: -e_size / 2,
+  borderRadius: e_size,
+  content: 'url(https://i.imgur.com/foXmE7c.png)',
+  textAlign: 'center',
+  fontSize: 16,
+  fontWeight: 'bold',
+  padding: 4,
+  cursor: 'pointer',
+};
+
+export const outerDivStyle = {
+  position: 'absolute',
+  width: e_size,
+  height: e_size,
+};
+
+export const userSpotStyle = {
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  left: -e_size / 2,
+  top: -e_size / 2,
+  borderRadius: e_size,
+  content: 'url(https://i.imgur.com/Xs70rpU.png)',
+  textAlign: 'center',
+  fontSize: 16,
+  fontWeight: 'bold',
+  padding: 4,
+  cursor: 'pointer',
+};
---
+++ client/src/components/userSpot.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { userSpotStyle, outerDivStyle } from './treasureStyle.js';
+
+export default class UserSpot extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+
+    var style = userSpotStyle;
+
+    return (
+      <div style={outerDivStyle}>
+        {this.props.user}
+        <div style={style}>
+        </div>
+      </div>
+    );
+  }
+}
