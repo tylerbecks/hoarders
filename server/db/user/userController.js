d8bae31c8f46cb7bfca25cbc834ad703853a32f2
isunnyvirk <isunnyvirk@gmail.com>
2016-07-28 11:53:26 -0700

    feat user data persists

--- server/db/user/userController.js
+++ server/db/user/userController.js
@@ -39,23 +39,55 @@ module.exports = {
   },
 
   updateUserPoints: (username, location,  socket) => {
+    var newLocA = location.substring(0, 6);
+    var newLocB = location.substring(7, 15);
+    var newLoc = String(newLocA) + String(newLocB);
+
     User.findOne({ username }, (err, userData) => { 
-      console.log('the userdata', userData)
       if (userData) {
         var flag = true;
-        for (var i = 0; i < userData.locations.length; i++) {
-          if (userData.locations[i] === location) {
-            flag = false;
-          }
-        }
-        if (flag) {
-          userData.locations.push(location);
-          userData.points++;
+        if (userData.locations.length < 1) {
+          console.log('this should happen very first time');
+          userData.locations.push(newLoc);
+          userData.points++; 
+          userData.markModified('locations', 'points');
+          userData.save(() => {
+            console.log('saving userdata');
+          });
           socket.emit('updateUserPoints', true);
         } else {
-          socket.emit('updateUserPoints', false);
+          for (var i = 0; i < userData.locations.length; i++) {
+            if (userData.locations[i] === newLoc) {
+              console.log('user already visited location');
+              socket.emit('updateUserPoints', false);
+              flag = false;
+            }
+          }
+          if (flag) {
+            console.log('user hasnt been here');
+            userData.locations.push(newLoc);
+            userData.points++;
+            userData.markModified('locations', 'points');
+            userData.save(() => {
+              console.log('saving new userData');
+            });
+            socket.emit('updateUserPoints', true);
+          } 
         }
       }
     });
+  },
+
+  getUserScore: (username, socket) => {
+    User.findOne({ username }, (err, userData) => {
+      console.log('the userdata is, ', userData);
+      if (userData) {
+        console.log('passing user data', userData.points);
+        socket.emit('getUserScore', userData.points); 
+      } else {
+        console.log('passing zero back', userData.points);
+        socket.emit('getUserScore', 0);
+      }
+    })
   },
 };
